5dafacc6e09885649ea0b87e20889792cfbb883a
architbubber <architbubber07@gmail.com>
2024-04-05 16:30:24 +0530

    Fixed Teams oage view for tab level device, alignment of menu button for mobile devices and added dropdown menu for mobile devices

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { NavigationStart, Router } from '@angular/router';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if(event instanceof NavigationStart) {
+        if(!$('#navbarId').hasClass('collapse')){
+          $('#navbarId').toggleClass('collapse');
+        }
+      }
 
+    });
+  }
+  openNavForMobile(){
+    $('#navbarId').toggleClass('collapse');
+  }
 }
