6320a2159efb33b9868498148326f880ca011f95
architbubber <architbubber07@gmail.com>
2024-04-07 13:59:58 +0530

    All images used are downloaded now and accessed from this repo itself

--- src/app/views/blogs/blogs.component.ts
+++ src/app/views/blogs/blogs.component.ts
@@ -25,14 +25,14 @@ export class BlogsComponent {
       id:1,
       date:'August 27, 2022',
       title:'Data-driven signal separation helps in identifying GIA signal in Alaska',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/Illustration_GIA_method.png',
+      imgSrc:'./assets/images/blogs/Illustration_GIA_method.png',
       description:"Our Earth is not rigid. In other words, it responds to surface mass load changes by deforming. During the last ice-age nearly 18000 yrs ago, most of the North America and Europe were under kilometers thick ice and this immense amount of load deformed the Earth. After the ice-age ended, these large ice-sheets melted and the load was lifted. The solid Earth surface is still responding to the change in load and this process is reffered to as the GIA or Glacial Isostatic Adjustment. Many regions in Canada and Europe are experiencing rapid uplift with respect to the Geocentre. This signal has a strong gravitational  component and is observed by GRACE. Hence to study hydrology this signal must be removed. The most common method to do that is to use GIA models that have huge uncertainties. Several researchers have thus tried to obtain data-driven estimates of GIA. Ina  recent work from th egroup, Bramha developed a data-driven method that uses data from GPS stations and GRACE in a novel mathematical framework that uses the geophysical relations to disentangle GIA and Surface mass change signal. The estimates from this work were significantly different from models over Alaska and Greenland. Many reesearchers have claimed that Alaska has a strong Visco-elastic signal that was missing from global GIA models. The results from this study have significant implications for studies focusing on sea level budget and Greenland ice-mass balance. You may read more about the work at: <https://academic.oup.com/gji/article/232/1/537/6674208>\n\n \n"
     },
     {
       id:2,
       date:'January 30, 2022',
       title:'Interpreting water storage trends from GRACE',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/Fig3_blogTVR-page-001.jpg',
+      imgSrc:'./assets/images/blogs/Fig3_blogTVR-page-001.jpg',
       description:'GRACE data has been abundantly used in research studies that want to capture a long-term water mass change, such as diminishing water bodies, droughts, and so on. The popular approach has been to first decompose the GRACE time series into seasonal signals and a linear trend, then by assuming that the magnitude of trend is directly proportional to the severity of water mass change, where regions with strong trends are investigated for the driving process (human-driven or climate change driven or extreme events). Since GRACE data covers less than two decades, any long term (multidecadal) natural hydrological variability would appear in the linear trend estimates. Furthermore, different regions have different natural variability cycles, for example, the ups and downs in water availability over a year in part of the Middle East (that is usually very arid) can be much smaller than in the Europe. This makes a comparison of trends between two regions very difficult. Hence, understanding the severity of linear trend obtained from a short time series, when the driving processes has a cyclic behaviour that lasts more than a decade can be challenging. This problem has been addressed in a recently published article ([https://doi.org/10.1088/1748-9326/abd4a9](https://iopscience.iop.org/article/10.1088/1748-9326/abd4a9)), where the authors propose a new metric to interpret the strength of trends with respect to long term natural spatiotemporal variability: Trend to variability ratio (TVR). It normalizes the total change in water mass in a region with the standard deviation of multidecadal natural hydrological variability obtained from simulations. TVR is an excellent tool for analysing the severity of short time series from other observational datasets.\n'
 
     },
@@ -40,7 +40,7 @@ export class BlogsComponent {
       id:3,
       date:'November 23, 2021',
       title:'Sea level rise is changing the sea floor',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/Illust_Ocean_mass_OBD.png',
+      imgSrc:'./assets/images/blogs/Illust_Ocean_mass_OBD.png',
       description:'Sea level rise is one of most dreadful consequences of the anthropogenic (caused by humans) global warming. It is estimated that the loss of coastal infrastructure alone would cost the world economy more than 1 trillion dollars per year by 2100. Researchers are closely monitoring changes in the sea level and trying to understand the physical processes driving these changes. Many studies have been able to explain the physical processes hat drive the global mean sea level change over the last few decades. The major takeaway from these studies was that in the second half of the 20th Century, steric sea level change (thermal expansion of oceans and salinity changes) was the dominant driver but in the last two decades mass change (due to addition of water from melting glaciers and ice-sheets on land) has significantly increased and is now the dominant component .\n\nBut additionally our solid Earth is not as rigid as it may seem. It is visco-elastic, which means when subjected to external force, it deforms, and when this external force is removed it resumes its shape, like a tennis ball. Since ice sheets (Greenland and Antarctica) are losing mass and oceans are gaining mass, Earth surface beneath the ice-sheets are slowly moving upward and the ocean bottom is going down due to changes in the surface mass (load). This change in the shape of the ocean floor is usually an order of magnitude smaller than the rate of change in ocean mass. Since the mass component of sea level rise was relatively small in the last half of the 20th century, OBD was negligible, but not anymore. The rate of sea level rise in the last two decades in approximately 3 mm/yr, out of which mass change constitutes 1.8 mm/yr, steric is 1.1 mm/yr and OBD is 0.1 mm/yr (see <https://doi.org/10.1029/2019GL086492> for more details).\n'
 
     },
@@ -48,7 +48,7 @@ export class BlogsComponent {
       id:4,
       date:'October 17, 2021',
       title:'The spatial resolution of GRACE products',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/GRACE_fil_fld.png',
+      imgSrc:'./assets/images/blogs/GRACE_fil_fld.png',
       description:'GRACE is a unique mission with unique design. It is not similar to optical remote sensing mission where we know the resolution of onboard sensor (or camera), rather it relies on geophysical inversion of the geopotential from the satellite range rate data (rate of change in the along-track distance between the two satellites in the same orbit).  Its spatial resolution cannot be expressed in terms of pixel size, or bands. In an article, the concept of the spatial resolution for a GRACE like mission was explained in detail (follow <https://www.mdpi.com/2072-4292/10/6/852>). In short, GRACE resolution and accuracy have a tradeoff and we can  expect GRACE to capture mass changes in catchments larger than 63000 sq. km with an RMSE (Root mean square error) of 2 cm EWH (equivalent water height).\n'
 
 
@@ -57,7 +57,7 @@ export class BlogsComponent {
       id:5,
       date:'September 14, 2021',
       title:'Filtering damages signal quality of GRACE products and we can counter it.',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/DDC.jpeg',
+      imgSrc:'./assets/images/blogs/DDC.jpeg',
       description:'GRACE data is noisy, which means filtering is essential to suppress noise. However, filtering also affects signal quality. In [https://doi.org/10.1002/2017WR021150,](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1002/2017WR021150,) authors describe a method that uses GRACE data only to estimate the signal loss due to filtering. They first describe the mathematical equations that define the signal loss due to filtering and then provide a neat data-driven method. This method is also compared with other signal restoration methods and was found to outperform them significantly.\n'
 
 
@@ -68,7 +68,7 @@ export class BlogsComponent {
       id:6,
       date:'August 15, 2021',
       title:'GRACE satellite mission',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/GRACE-920x450.jpg',
+      imgSrc:'./assets/images/blogs/GRACE-920x450.jpg',
       description:'Availability of good quality data is essential to deduce meaningful information in all disciplines of science and technology. This statement is especially relevant in climate research, which is under constant scrutiny from sceptics. In order to better understand the Earth system response to the ongoing anthropogenic global warming, we require global observations of climate indicators at very high spatiotemporal resolutions. In a perfect world, every country provides a dense observation network and shares its data openly for research. Yet political and financial constraints prohibit this reality.\n\nHence, the era of satellite-based Earth observation has been a revolution for geosciences. The launch of satellites that observe our planet since the 1970s, such as optical remote sensing satellites, satellite altimetry launched in the early 1990s followed by the gravity recovery and climate research (GRACE) missions in 2002, provided a much needed momentum to climate change research. Most importantly, GRACE has been a game changer. It consists of two satellites, separated by 220 km with one behind the other in the same near-polar orbit, and by measuring changes in the distance between the two satellite with micro-meter precision, we are able to map the changes in the gravity field of the Earth from one month to the other. These very small changes in local gravity field are driven by mass redistribution in the Earth system. For example, if it rains heavily and the water is collected in an adjacent (sub-surface or on surface) water body, an increase in mass is recorded in the gravity data from the satellite.\xa0 Similarly, for droughts or ice-sheet melting, a mass loss perturbs the gravity field and will be recorded by the satellite mission. As a result, GRACE data has been used in nearly all the disciplines of Earth sciences where we are concerned with mass redistribution. However, the impact of GRACE on hydrology is unmatched.\n'
 
     }
--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -10,26 +10,26 @@ export class ProjectsComponent {
     {
       title:'TERRESTRIAL WATER CYCLE',
       heading:'Accounting and auditing every drop',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/MScR_illust_fig.png',
+      imgSrc:'./assets/images/projects/MScR_illust_fig.png',
       text:'It is accepted that the water cycle will intensify as the globe warms, however how this will change locally is still under debate. One of the major drawbacks in all the traditional approaches/methods that model the water cycle or study water-energy partioning, is that they assume water storage changes to be zero. It was an assumption to counter the fact that no data was available on water storage prior to 2002. However, since the launch of GRACE satellite mission in 2002, we can now measure the terrestrial water storage changes and it can help us better understand the changes in water cycle. We are using GRACE data for: estimating one of the water budget component when all others are known, validating model simulations of a budget component, estimating groundwater changes, and for understanding the impact of climate and human intervention. Vivek, Balaram, Pushkar and Abhishek are working on various topics related to this theme.'
     },
     {
       title:'SATELLITE DATA PROCESSING & DATA ASSIMILATION',
       heading:'Improving Earth observation data',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/AMZ_fig.png',
+      imgSrc:'./assets/images/projects/AMZ_fig.png',
       text:'Different datasets have different strengths. For example, in-situ point data have excellent resolution but are sparse and costly to maintain, thus cannot provide a consistent large scale overview. On the other hand, satellite remote sensing are excellent in terms of their spatial coverage but have poor spatiotemporal resolution. In addition, they require explicit postprocessing and calibration. Geo-physical or Hydrological models on the other hand can have high resolution but with large uncertainties. Therefore, merging them can help us obtain optimal products that may benefit policy and research. With this motivation, we also employ satistical downscaling techniques, Kalman filtering, and machine learning to solve problems of scale in Earth observation. Amin, Vasaw (an exchange student from IIT B) and Vivek are working in this domain.\
       We also have expertise in processing Geodetic observations to derive meaningful Earth observation variables and are interested in developing algorithms for satellite data processing.'
     },
     {
       title:'SEA LEVEL RISE',
       heading:'Coastal sea level changes',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/sea_level_review_fig.png',
+      imgSrc:'./assets/images/projects/sea_level_review_fig.png',
       text:'Our knowledge and understanding of global mean sea level rise is excellent. However the sea level rise in not uniform and can vary from place to place. This spatial variability makes a few costal regions more vulnerable to climate-driven sea level rise than others. The coastal sea level rise is traditionally monitored with tide-gauges that are sparse and not of consistent quality. Therefore, relying on remote sensing is the only viable option but their performance over coastal oceans is also not great. Recent developments have helped scientists to produce satellite-altimetery based coatsl sea level products. However, their validation is still an ongoing task and we are trying to contribute in that direction. Vandana is working on this project.      Additionally, we also study the sea level budget componenets and we are currently investigating the drivers of sea level in various ocean basins.'
     },
     {
       title:'THE CRYOSPHERE',
       heading:'Shrinking glaciers and ice-sheet',
-      imgSrc:'https://ultra-pluto-7f6d1.stackbit.app/images/Fig1.jpg',
+      imgSrc:'./assets/images/projects/Fig1.jpg',
       text:'Changes in frozen water is an excellent indicator of global warming. We are currently focusing on understanding the spatiotemporal behaviour of glaciers in the LAdhak region in the Indian Himalayas. We collect data in-situ as well as we employ remote sensing. In the past, we have used GRACE data to obtain mass change time-series for Greenland and Antractica. The glaciological part is led by Arindan.'
     }
   ]
